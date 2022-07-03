f766dd50ca8b2ac6a0c24ee9a003e3f9e87ae9c5
shakir <shakirmuhammetov@gmail.com>
2022-07-03 17:59:00 +0600

    зарегистрировал приложения shop сделал миграции и зарегистрировал в админке

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from shop.models import Product
+
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category', 'price', 'balance']
+    list_display_links = ['name']
+    list_filter = ['category', ]
+    search_fields = ['name', 'category', ]
+    fields = ['name', 'category', 'price', 'balance', ]
+
+
+#
+admin.site.register(Product, ShopAdmin)
