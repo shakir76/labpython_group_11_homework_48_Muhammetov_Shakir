ca99a785b2c29ebc5467c0c8ccac4c01cc42a68c
shakir <shakirmuhammetov@gmail.com>
2022-07-04 22:20:07 +0600

    Добавил функцию вывода детального просмотра

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,8 @@
-
 from django.urls import path, include
 
-from shop.views import index
+from shop.views import index, product_view
 
 urlpatterns = [
-    path('', index, name="index")
+    path('', index, name="index"),
+    path('product/<int:pk>/', product_view, name="view")
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
-
 # Create your views here.
 from shop.models import Product
 
@@ -9,3 +8,9 @@ def index(request):
     product = Product.objects.order_by("category")
     context = {"products": product}
     return render(request, "index.html", context)
+
+
+def product_view(request, **kwargs):
+    pk = kwargs.get("pk")
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "product_view.html", {'product': product})
