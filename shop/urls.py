fcc41b688571ca40dd7af8004eedf1add1d2e8b5
shakir <shakirmuhammetov@gmail.com>
2022-07-04 23:00:25 +0600

    Добавил редактированния продукта

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 
-from shop.views import index, product_view, create_product
+from shop.views import index, product_view, create_product, update_product
 
 urlpatterns = [
     path('', index, name="index"),
     path('product/<int:pk>/', product_view, name="view"),
-    path('product/create/', create_product, name="create")
+    path('product/create/', create_product, name="create"),
+    path('product/update/<int:pk>/', update_product, name="update"),
+    # path('product/delete/<int:pk>/', delete_product, name="delete")
 ]
--- shop/views.py
+++ shop/views.py
@@ -30,6 +30,39 @@ def create_product(request):
             price = form.cleaned_data.get("price")
             description = form.cleaned_data.get("description")
             Product.objects.create(name=name, category=category, balance=balance, price=price,
-                                              description=description)
+                                   description=description)
             return redirect("index")
         return render("create.html", {"form": form})
+
+
+def update_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "GET":
+        form = ProductForm(initial={
+            "name": product.name,
+            "category": product.category,
+            "balance": product.balance,
+            "description": product.description,
+            "price": product.price
+        })
+        return render(request, "update.html", {'form': form, 'category': STATUS_CODE})
+    else:
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            product.name = form.cleaned_data.get("name")
+            product.category = form.cleaned_data.get("category")
+            product.balance = form.cleaned_data.get("balance")
+            product.price = form.cleaned_data.get("price")
+            product.description = form.cleaned_data.get("description")
+            product.save()
+            return redirect("view", pk=product.pk)
+        return render(request, "update.html", {'form': form, 'category': STATUS_CODE})
+
+
+def delete_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == "GET":
+        return render(request, "delete.html", {"product": product})
+    else:
+        product.delete()
+        return redirect("index")
